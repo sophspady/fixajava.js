bd495d44769dfff60d92a60b2769e6be7215d022
soph <132591418+sophspady@users.noreply.github.com>
2023-05-30 11:36:38 -0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,72 @@
+// var turno  = prompt ("em qual turno voce estuda")
+// if ( turno == "matutino" || turno == "M" || turno == "m"){
+// alert("bom dia") 
+// } 
+// else if ( turno == "vespertino" || turno== "V" || turno == "v" ){
+// alert ("boa tarde")
+// } 
+// else if (turno == "noturno" || turno== "n" || turno == "N"){
+// alert ("boa noite")
+// }
+
+// var nota1 = Number(prompt ("qual é a sua primeira nota?"))
+// var nota2 = Number(prompt ("qual a sua segunda nota?"))
+// var media = (nota1 + nota2)/2 
+// if (media >= 7 && media < 10 ) {
+//  alert ("aprovado")
+// }
+// else if ( media < 7){
+//  alert ("reprovado")
+// }
+// else if ( media ==10) {
+//  alert ("aprovado com distinção")
+// }
+
+
+// var nota1 = Number(prompt("qual é a sua primeira nota?"))
+// var nota2 = Number(prompt(" qual é a sua segunda nota?"))
+// var media = (nota1 + nota2)/2
+// if ( media ==9 && media ==10){
+//     alert ("A")
+// }
+// else if (media >=7,5 && media <=9){
+//     alert ("B")
+// }
+// if (media >=6 && media <=7,5){
+//     alert ("C")
+// }
+// else if (media >=4 && media <=6){
+//     alert ("D")
+// }
+// if (media <4 && media ==0){
+//     alert ("E")
+// }
+// else if (media == "A" || media == "B" || media == "C"){
+//     alert("sua media foi" + media + "voce foi aprovado!")
+// }
+// if (media == "D" || media == "C"){
+// alert ("sua media foi" + media + "voce foi reprovado")
+// };
+
+
+// var idade=prompt ("qual a sua idade?")
+//  if (idade <16){
+//     alert (" voce nao pode votar")
+// }
+// else if ( idade >=16){
+// alert (" voce pode votar")
+// };
+
+var coisa = window.document.write ("<h1> Olá <h1>")
+
+// window.document.getElementsByTagName("p")[24]
+
+
+
+
+
+
+
+
+
+
