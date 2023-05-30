659df2b10f0a52ed0e85928c17a1d597e0e3f7ee
soph <132591418+sophspady@users.noreply.github.com>
2023-05-30 15:48:09 +0000

    k

--- java/script.js
+++ java/script.js
@@ -68,6 +68,11 @@ const user = document.getElementsByTagName("input")[0]
 const senha = document.getElementsByTagName ("input")[1].value
 alert("eu sou" + user + senha)
 
+if (user == "soph" && senha == "123"){
+    alert (" boa")
+}else{
+    alert ("não")
+}
 }
 
 
