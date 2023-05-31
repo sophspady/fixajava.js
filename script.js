1b2f9491cc83046c31ca696e1cd30fda86224dac
soph <132591418+sophspady@users.noreply.github.com>
2023-05-31 17:37:01 +0000

    agora sim pô

--- script.js
+++ script.js
@@ -138,12 +138,15 @@ switch (operaçao) {
     case "2":
         res = n1 - n2
         alert(res)
+        break
     case "3":
         res = n1 * n2
         alert(res)
+        break
     case "4":
         res = n1 / n2
         alert(res)
+        break
     default:
         alert("valor inválido!")
 }
